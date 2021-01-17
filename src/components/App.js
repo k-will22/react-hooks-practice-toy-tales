0c0cace41562269eafc075b7489b4f673073d229
k-will22 <thestreetkw@hotmail.com>
2021-01-17 00:01:33 -0500

    commit tree ccd751f3f93f660798fde8b607be37a52b56baa4

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,19 +6,49 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([])
+
+useEffect(() => {
+  fetch("http://localhost:3001/toys")
+  .then(response => response.json())
+  .then(data => {
+    setToys(data)
+  })
+}, [])
+
+console.log(toys)
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  function handleUpdateToy(updatedToy) {
+    const updatedToys = toys.map((toy) =>
+    toy.id === updatedToy.id ? updatedToy : toy)
+    setToys(updatedToys)
+  }
+
+  function handleDeleteToy(deletedToy) {
+    const updatedToys = toys.filter((toy) => 
+    toy.id !== deletedToy.id)
+    setToys(updatedToys)
+  }
+
+  function handleAddToy(newToy) {
+    setToys([...toys, newToy])
+  }
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm onAddToy={handleAddToy} /> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer 
+        toys={toys} 
+        onUpdateToy={handleUpdateToy}
+        onDeleteToy={handleDeleteToy} />
     </>
   );
 }
--- src/components/ToyCard.js
+++ src/components/ToyCard.js
@@ -1,17 +1,43 @@
 import React from "react";
 
-function ToyCard() {
+function ToyCard({ toy, onUpdateToy, onDeleteToy }) {
+  const { id, name, image, likes } = toy
+
+  function handleDelete() {
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "DELETE",
+    })
+    .then(response => response.json)
+    .then(() => {
+      onDeleteToy(toy)
+    })
+  }
+
+  function handleLikes() {
+    const newObj = {likes : likes + 1}
+
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newObj),
+    })
+    .then(response => response.json())
+    .then(onUpdateToy)
+
+  }
   return (
     <div className="card">
-      <h2>{"" /* Toy's Name */}</h2>
+      <h2>{name}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={image}
+        alt={name}
         className="toy-avatar"
       />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <p>{toy.likes} Likes </p>
+      <button onClick={handleLikes} className="like-btn">Like {"<3"}</button>
+      <button onClick={handleDelete} className="del-btn">Donate to GoodWill</button>
     </div>
   );
 }
